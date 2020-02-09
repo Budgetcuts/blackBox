419f97bca73d61eb7560af7fcbc30b73d2cd5c57
sblack3 <sblack3@binghamton.edu>
2020-02-08 19:26:18 -0500

    update

--- box.py
+++ box.py
@@ -1,11 +1,16 @@
 import numpy as np
 import coin
 import ledger
+import peer
+
+def partition(coinValue, systemNumber):
+    print("Hi")
+
 
 
 def main():
     print("Jonah is a coward")
-    c = coin.Coin(2)
+    c = coin.Coin(1,1,1)
     c.string()
    
 
--- coin.py
+++ coin.py
@@ -1,5 +1,33 @@
 class Coin:
-    def __init__(self, x):
-        self.x = x
+    def __init__(self, iComp, avgComp, sysNum):
+        self.iComp = iComp
+        self.avgComp = avgComp
+        self.sysNum = sysNum
+
+
+    def addCoins(self):
+        pVal = self.iComp/(self.avgComp * self.sysNum)
+        return pVal
+
+    def coinVal(self):
+        oVal = self.avgComp * self.sysNum
+        return oVal
+
+    def updateAll(self,iComp, avgComp, sysNum):
+        self.iComp = iComp
+        self.avgComp = avgComp
+        self.sysNum = sysNum
+
+    def updateMyComp(self, iComp):
+        self.iComp = iComp
+
+    def updateSysComp(self, avgComp):
+        self.avgComp = avgComp
+
+    def updateUsers(self, sysNum):
+        self.sysNum = sysNum
+
     def string(self):
-        print("hi")
+        print("Personal Computational Power:", self.iComp)
+        print("Average Computational Power of the network:", self.avgComp)
+        print("Number of active systems on the network:", self.sysNum)
