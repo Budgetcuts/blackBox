a081b81ec7ad2219aedc11a1be3aeb511498e4a2
sblack3 <sblack3@binghamton.edu>
2020-02-08 21:31:56 -0500

    update

--- box.py
+++ box.py
@@ -4,6 +4,10 @@ import ledger
 import peer
 import gui
 
+from kivy.app import App
+from kivy.uix.button import Button
+
+
 def partition(coinValue, pAvg, systemNumber, totalProcess):
     uProcess = totalProcess/1.1
     iPart = 1
@@ -15,12 +19,19 @@ def partition(coinValue, pAvg, systemNumber, totalProcess):
             bool = True
     
 
+def update():
+    while(True):
+        myCoin = coin.Coin(1,1,1)
+
+
 
 def main():
+
     t = gui.Gui()
     t.run()
     c = coin.Coin(1,1,1)
     c.string()
+    update()
    
 
 main()
