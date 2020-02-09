beba5c7e5cfadcf696670e61c8176e9d0bdd180d
sblack3 <sblack3@binghamton.edu>
2020-02-08 19:35:07 -0500

    update

--- box.py
+++ box.py
@@ -3,9 +3,16 @@ import coin
 import ledger
 import peer
 
-def partition(coinValue, systemNumber):
-    print("Hi")
-
+def partition(coinValue, pAvg, systemNumber, totalProcess):
+    uProcess = totalProcess/1.1
+    iPart = 1
+    bool = False
+    while(not bool):
+        if(uProcess < iPart):
+            iPart/1.001
+        else:
+            bool = True
+    
 
 
 def main():
